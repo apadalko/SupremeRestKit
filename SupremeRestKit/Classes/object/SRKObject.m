f21650a840a1b74b85f4aa080d2293be55edc71e
Alex Padalko <a.padalko@icloud.com>
2017-06-07 17:06:23 -0400

    some fix

--- SupremeRestKit/Classes/object/SRKObject.m
+++ SupremeRestKit/Classes/object/SRKObject.m
@@ -433,23 +433,23 @@ static NSNumberFormatter *_numberFormatter;
 - (void)setObject:(id)object forKey:(NSString *)key {
     
     
-    NSString *sourceString = [[NSThread callStackSymbols] objectAtIndex:1];
-    // Example: 1   UIKit                               0x00540c89 -[UIApplication _callInitializationDelegatesForURL:payload:suspended:] + 1163
-    NSCharacterSet *separatorSet = [NSCharacterSet characterSetWithCharactersInString:@" -[]+?.,"];
-    NSMutableArray *array = [NSMutableArray arrayWithArray:[sourceString  componentsSeparatedByCharactersInSet:separatorSet]];
-    [array removeObject:@""];
-    
-    NSLog(@"Stack = %@", [array objectAtIndex:0]);
-    NSLog(@"Framework = %@", [array objectAtIndex:1]);
-    NSLog(@"Memory address = %@", [array objectAtIndex:2]);
-    NSLog(@"Class caller = %@", [array objectAtIndex:3]);
-    NSLog(@"Function caller = %@", [array objectAtIndex:4]);
-    
-    NSString * _key = [NSString stringWithFormat:@"%C%@",
-     (unichar)toupper([key characterAtIndex:0]),
-                       [key substringFromIndex:1]];
-    
-    NSString * fullSelector = [NSString stringWithFormat:@"set%@:",_key];
+//    NSString *sourceString = [[NSThread callStackSymbols] objectAtIndex:1];
+//    // Example: 1   UIKit                               0x00540c89 -[UIApplication _callInitializationDelegatesForURL:payload:suspended:] + 1163
+//    NSCharacterSet *separatorSet = [NSCharacterSet characterSetWithCharactersInString:@" -[]+?.,"];
+//    NSMutableArray *array = [NSMutableArray arrayWithArray:[sourceString  componentsSeparatedByCharactersInSet:separatorSet]];
+//    [array removeObject:@""];
+//    
+//    NSLog(@"Stack = %@", [array objectAtIndex:0]);
+//    NSLog(@"Framework = %@", [array objectAtIndex:1]);
+//    NSLog(@"Memory address = %@", [array objectAtIndex:2]);
+//    NSLog(@"Class caller = %@", [array objectAtIndex:3]);
+//    NSLog(@"Function caller = %@", [array objectAtIndex:4]);
+//    
+//    NSString * _key = [NSString stringWithFormat:@"%C%@",
+//     (unichar)toupper([key characterAtIndex:0]),
+//                       [key substringFromIndex:1]];
+//    
+//    NSString * fullSelector = [NSString stringWithFormat:@"set%@:",_key];
     
     
 //    [NSInvocation invocationWithMethodSignature:<#(nonnull NSMethodSignature *)#>]
